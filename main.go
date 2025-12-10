8bfd8b7eff5ce27a450ad90185398c03701c2d35
Akvilion <ianko.andrii@gmail.com>
2025-12-10 11:26:09 +0200

    1

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+func main() {
+	err := wails.Run(&options.App{
+		Title:  "Pica3",
+		Width:  800,
+		Height: 600,
+		// Повністю прозорий фон (A: 0 = прозорий, A: 255 = непрозорий)
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 0},
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		// Специфічні налаштування для OS
+		Mac: &mac.Options{
+			TitleBar:             mac.TitleBarHiddenInset(),
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			// BackdropType можна закоментувати для повної прозорості
+			// BackdropType: windows.Acrylic,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
